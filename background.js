d7db46f3a50bdb5aeb31bbfa72bdf65d859219fc
bran <112417913+blulw@users.noreply.github.com>
2024-12-15 00:01:22 +0000

    oliver

--- background.js
+++ background.js
@@ -7,37 +7,32 @@
 //             }
 //     }
 
-chrome.runtime.onMessage.addListener(data =>
-{
-    const { prefs } = data
-    switch (data.event)
-    {
-        case 'onEnable':
-            handleOnEnable();
-            break;
-        case 'onDisable':
-            handleOnDisable();
-            break;
-        case 'onSwitchOpacity':
-            handleOnSwitchOpacity(prefs);
-            break;
-        default:
-            break;
-    }
-})
+chrome.runtime.onMessage.addListener((data) => {
+  const { prefs } = data;
+  switch (data.event) {
+    case "onEnable":
+      handleOnEnable();
+      break;
+    case "onDisable":
+      handleOnDisable();
+      break;
+    case "onSwitchOpacity":
+      handleOnSwitchOpacity(prefs);
+      break;
+    default:
+      break;
+  }
+});
 
-const handleOnEnable = () =>
-{
-    console.log("Enabled")
-}
+const handleOnEnable = () => {
+  console.log("Enabled");
+};
 
-const handleOnDisable = () =>
-{
-    console.log("Disabled")
-}
+const handleOnDisable = () => {
+  console.log("Disabled");
+};
 
-const handleOnSwitchOpacity = (prefs) =>
-{
-    console.log("prefs for opacity received: ", prefs)
-    chrome.storage.local.set(prefs)
-}
+const handleOnSwitchOpacity = (prefs) => {
+  console.log("prefs for opacity received: ", prefs);
+  chrome.storage.local.set(prefs);
+};
